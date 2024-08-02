422dc023bd40ae20606f58f8051868a206568489
Den Pol <selection4test@gmail.com>
2024-08-02 16:54:25 +0300

    fix

--- src/utils/person-ops/getFullName.ts
+++ src/utils/person-ops/getFullName.ts
@@ -1,12 +1,12 @@
 import { TPresonDataResponse } from "~/types"
 import cn from 'clsx'
 
-export const getFullName = (personData: TPresonDataResponse): string => {
-  return personData.data.customService?.data.baseInfo
+export const getFullName = (personData: TPresonDataResponse | null): string => {
+  return personData?.data.customService?.data.baseInfo
     ? cn(
-      personData.data.customService?.data.baseInfo.firstName,
-      personData.data.customService?.data.baseInfo.middleName,
-      personData.data.customService?.data.baseInfo.lastName,
+      personData?.data.customService?.data.baseInfo.firstName,
+      personData?.data.customService?.data.baseInfo.middleName,
+      personData?.data.customService?.data.baseInfo.lastName,
     )
-    : (personData.message || 'Full Name ERR #0 (no data)')
+    : (personData?.message || 'Full Name ERR #0 (no data)')
 }
